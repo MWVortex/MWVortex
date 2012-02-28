d7ddcd8bae5ec25d917bef1e79d655dc8d6b26c8
Nicolas Foisy <foisynicolas@gmail.com>
2012-02-27 21:08:57 -0500

    lsdfhja

--- src/Vortex.java
+++ src/Vortex.java
@@ -4,10 +4,8 @@ public class Vortex {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         //allo
-=======
-        
->>>>>>> a347a748af9fa3c86f493f28e32776b5feed67df
+	//bazwell
     }
 }
