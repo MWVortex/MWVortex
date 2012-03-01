2e325f15822e883e8e47aaa2785b5fc9e3badbe4
Sophie Déziel <courrier@sophiedeziel.com>
2012-03-01 15:37:38 -0500

    Création des fonctions

--- src/Configurable.java
+++ src/Configurable.java
@@ -1,12 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author sophie
+ * 
+ * @author Sophie Déziel
+ * 
  */
 public interface Configurable {
+    
+    abstract void importer();
+    abstract void exporter();
     
 }
--- src/Membres.java
+++ src/Membres.java
@@ -1,12 +1,60 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
+ * Gère les membres qui ont accès au logiciel
+ * @author Sophie Déziel 
  *
- * @author sophie
  */
-public class Membres {
-    
+public class Membres implements Configurable {
+
+    @Override
+    public void importer() {
+    }
+
+    @Override
+    public void exporter() {
+    }
+
+    /**
+     * Retire un membre
+     *
+     * @return boolean true si successful
+     */
+    public boolean retirer(String pseudo) {
+        return false;
+    }
+
+    /**
+     *Ajoute un membre
+     * @param pseudo
+     * @param pass
+     * @return boolean, true si l'ajout s'est bien effectué
+     */
+    public boolean ajouter(String pseudo, String pass) {
+        return false;
+    }
+
+    /**
+     * 
+     * @param pseudo
+     * @param pass
+     * @return true lorsque l'utilisateur existe et a le bon mot de passe
+     */
+    public boolean login(String pseudo, String pass) {
+        return false;
+    }
+    /**
+     * Vérifie que le membre existe bien
+     * @param pseudo
+     * @return 
+     */
+    public boolean existe(String pseudo) {
+        return false;
+    }
+    /**
+     * Vérifie si le membre a les droits administrateurs
+     * @return 
+     */
+    public boolean estAdmin() {
+        return false;
+    }
 }
