919e9cafc3ba0a76189ddbf1fa9cd8c424ef1af4
isaacbacon1 <isaac_bacon@hotmail.com>
2012-03-01 15:21:40 -0500

    version updater

--- src/Song.java
+++ src/Song.java
@@ -7,13 +7,33 @@
  *
  * @author Arcturus
  */
+import java.io.File;
+import org.jaudiotagger.tag.id3.ID3v1Tag;
 public class Song {
-    private String song = "";
-    private ArrayList<Album> albums;
     
-    public Artist(){
-        song = this.song;
-        albums = this.albums;
+    int track = 0;
+    private String title = "";
+    private String artist = "";
+    private String album = "";
+    private String year = "";
+    private String label = "";
+    private String composer = "";
+    private File path = null;
+    
+    public Song(){
+        buildSong(); //popule les variables d'instance
+        
+        track = this.track;
+        title = this.title;
+        artist = this.artist;
+        album = this.album;
+        year = this.year;
+        label = this.label;
+        composer = this.composer;
+        path = this.path;
     }
-}
+    
+    private void buildSong(){
+        //TODO
+    }
 }
