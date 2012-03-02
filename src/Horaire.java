a1b851205145e6f18fe12ce1785c7d9f6ed5f5f3
Nicolas <Nicolas@Nico>
2012-03-02 09:54:50 -0500

    validationHoraire + XML

--- src/Horaire.java
+++ src/Horaire.java
@@ -1,22 +1,43 @@
-
-import java.beans.XMLEncoder;
-import java.io.*;
-
-
 public class Horaire implements Temps {
     
-    public String creerPlaylist()throws IOException{
-        
-        String playList = "termine";
-        
-        XMLEncoder e = new XMLEncoder(
-                            new BufferedOutputStream(
-                            new FileOutputStream("Test.xml")));
-       e.writeObject("test");
-       e.close();
-        
-        //playList = 
-        return playList;
-        
+    private String nomDj; 
+    private String startTime;
+    private String duration;
+
+    /**
+     * Créer une nouvel plage horaire
+     */
+    public Horaire (String nomDj, String startTime, String duration ){
+        setNomDj( nomDj );
+        setStartTime( startTime );
+        setDuration( duration );      
+    } 
+    
+    /**
+     * Modifie le nom du DJ. 
+     */
+    public void setNomDj( String nomDj ){
+        this.nomDj = nomDj;
+    }
+    
+    /**
+     * Modifie le startTime. 
+     */
+    public void setStartTime( String startTime ){
+        this.startTime = startTime;
+    }  
+    
+    /**
+     * Modifie la durée. 
+     */
+    public void setDuration( String duration ){
+        this.duration = duration;
+    } 
+
+   /**
+    * Méthode qui génère du XML dans un fichier à partir des données recueillies 
+    */
+    public String creerXml(){ 
+      return("");
     } 
 }
