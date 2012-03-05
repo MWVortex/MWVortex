d2379570240998714eb9990c333f00c755512afa
Isaac Bacon <isaacbacon1@gmail.com>
2012-03-04 20:02:15 -0500

    as

---
+++ src/MP3Collection.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author Phenix
+ */
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class MP3Collection {
+    public static ArrayList<Path> createMP3DirectoryMap(){
+        ArrayList<Path> pathList = new ArrayList<Path>();
+        String mp3DirName = "C://MP3/";
+        
+        
+        
+        return pathList;
+    }
+}
